40acebd30a446b52d99064b20ef1a28499dbdf98
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-08-04 13:33:14 +0100

    tmp

--- tests/ui-tests/playwright.config.ts
+++ tests/ui-tests/playwright.config.ts
@@ -17,7 +17,7 @@ dotenv.config({ path: path.resolve(__dirname, ".env") });
 
 export default defineConfig({
   testDir,
-  globalSetup: "./global-setup.ts",
+  globalSetup: env.TEST_TARGET === "local" ? "./global-setup.ts" : undefined,
   forbidOnly: !env.HUMAN_IN_THE_LOOP,
   preserveOutput: "failures-only",
   workers: "50%",
