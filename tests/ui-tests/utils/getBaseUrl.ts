808b5904666036459a80e6d446fb38a049d1a15c
Louisa Nash <louisa.nash@digital.cabinet-office.gov.uk>
2026-08-04 11:03:17 +0100

    tmp

--- tests/ui-tests/utils/getBaseUrl.ts
+++ tests/ui-tests/utils/getBaseUrl.ts
@@ -1,3 +1,13 @@
+import { env } from "../env";
+
 export const getBaseUrl = () => {
-  return "http://localhost:6001"; // TODO: Update this for dev and build to point to running ECS instance
+  if (env.TEST_TARGET === "local") {
+    return "http://localhost:6001/";
+  }
+
+  if (env.TEST_TARGET === "production") {
+    return "https://manage.sign-in.service.gov.uk";
+  }
+
+  return `https://manage.${env.TEST_TARGET}.sign-in.service.gov.uk`;
 };
